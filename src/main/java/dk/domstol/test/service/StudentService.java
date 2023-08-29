6fb0872de7a0a95fcbe3f23a01bcf10f87c6739a
Hans Seeberg <vi-b-hse@domstolsstyrelsen.dk>
2023-08-29 11:29:59 +0200

    Test with graduates

--- src/main/java/dk/domstol/test/service/StudentService.java
+++ src/main/java/dk/domstol/test/service/StudentService.java
@@ -2,10 +2,12 @@ package dk.domstol.test.service;
 
 import dk.domstol.test.model.Freshman;
 import dk.domstol.test.model.Graduate;
+import dk.domstol.test.model.Graduate_;
 import dk.domstol.test.model.School;
 import dk.domstol.test.model.School_;
 import dk.domstol.test.repository.Repository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.Join;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
@@ -31,8 +33,8 @@ public class StudentService {
         var query = builder.createQuery(School.class);
         var root = query.from(School.class);
         root.fetch(School_.STUDENTS);
-        root.fetch(School_.HELPER);
-
+        var helper =  (Join<Object, Object>)root.fetch(School_.HELPER);
+        query.where(builder.equal(helper.get(Graduate_.NAVN),"mig"));
 
         var list2 = entityManager.createQuery(query).getResultList();
     }
